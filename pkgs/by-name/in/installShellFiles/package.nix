22c36b72e5103bbf901f45aedcdabac398b8fa34
Stefan Frijters <sfrijters@gmail.com>
2025-01-11 02:48:13 +0100

    WIP

--- pkgs/by-name/in/installShellFiles/package.nix
+++ pkgs/by-name/in/installShellFiles/package.nix
@@ -4,6 +4,8 @@
   callPackage,
   makeSetupHook,
   hostPlatform_ ? null,
+  allowEmulator ? true,
+  allowBuildPackages ? true,
   buildPackages
 }:
 
@@ -12,7 +14,7 @@ makeSetupHook {
   name = "install-shell-files";
   substitutions = {
     canExecute = if hostPlatform_ != null then lib.debug.traceValFn (v: "canExecute ${builtins.toString v}") (if stdenv.buildPlatform.canExecute hostPlatform_ then 1 else 0) else 0;
-    emulatorAvailable = if hostPlatform_ != null then lib.debug.traceValFn (v: "emulatorAvailable ${builtins.toString v}") (if hostPlatform_.emulatorAvailable buildPackages then 1 else 0) else 0;
+    emulatorAvailable = if hostPlatform_ != null then lib.debug.traceValFn (v: "emulatorAvailable ${builtins.toString v}") (if hostPlatform_.emulatorAvailable buildPackages && allowEmulator then 1 else 0) else 0;
     emulator = if hostPlatform_ != null then lib.debug.traceVal (hostPlatform_.emulator buildPackages) else 0;
     buildPlatform = lib.debug.traceValFn (v: "buildPlatform ${v}") stdenv.buildPlatform.config;
     hostPlatform = if hostPlatform_ != null then lib.debug.traceValFn (v: "hostPlatform ${v}") hostPlatform_.config else "";
