b4a3842ade3fa07cebeeb968636de3f611dd0f80
Stefan Frijters <sfrijters@gmail.com>
2025-12-18 10:07:01 +0100

    Revert "libxmlb: fix cyclic reference with __structuredAttrs"
    This reverts commit 90cdc035b4e1310f0b18020b2c193d84a1153794.

--- pkgs/by-name/li/libxmlb/package.nix
+++ pkgs/by-name/li/libxmlb/package.nix
@@ -46,12 +46,6 @@ stdenv.mkDerivation rec {
     ./installed-tests-path.patch
   ];
 
-  postPatch = lib.optionalString withIntrospection ''
-    # If we don't export this, $lib/lib/girepository-1.0/Xmlb-2.0.typelib
-    # will be generated with a reference to $out
-    export outputLib
-  '';
-
   nativeBuildInputs = [
     docbook_xml_dtd_43
     docbook-xsl-nons
@@ -90,8 +84,6 @@ stdenv.mkDerivation rec {
       installed-tests = nixosTests.installed-tests.libxmlb;
     };
   };
-
-  __structuredAttrs = true;
 
   meta = {
     description = "Library to help create and query binary XML blobs";
