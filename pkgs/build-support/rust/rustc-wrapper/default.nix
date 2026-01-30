b36799738cdccc576de996898524dd8cd16c8a35
Stefan Frijters <sfrijters@gmail.com>
2026-01-30 11:56:46 +0100

    build-support/rustc-wrapper: use replaceVars instead of substituteAll

--- pkgs/build-support/rust/rustc-wrapper/default.nix
+++ pkgs/build-support/rust/rustc-wrapper/default.nix
@@ -1,39 +1,51 @@
 {
   lib,
   runCommand,
+  replaceVars,
   rustc-unwrapped,
   sysroot ? null,
+  stdenvNoCC,
+  shell ? stdenvNoCC.shell,
 }:
+let
+  sysrootFlag = lib.optionalString (sysroot != null) "--sysroot ${sysroot}";
+  # Upstream rustc still assumes that musl = static[1].  The fix for
+  # this is to disable crt-static by default for non-static musl
+  # targets.
+  #
+  # Even though Cargo will build build.rs files for the build platform,
+  # cross-compiling _from_ musl appears to work fine, so we only need
+  # to do this when rustc's target platform is dynamically linked musl.
+  #
+  # [1]: https://github.com/rust-lang/compiler-team/issues/422
+  #
+  # WARNING: using defaultArgs is dangerous, as it will apply to all
+  # targets used by this compiler (host and target).  This means
+  # that it can't be used to set arguments that should only be
+  # applied to the target.  It's fine to do this for -crt-static,
+  # because rustc does not support +crt-static host platforms
+  # anyway.
+  defaultArgs = lib.optionalString (
+    with rustc-unwrapped.stdenv.targetPlatform; isMusl && !isStatic
+  ) "-C target-feature=-crt-static";
 
+  rustc-wrapper = replaceVars ./rustc-wrapper.sh {
+    inherit shell sysrootFlag defaultArgs;
+    prog = "${lib.getExe' rustc-unwrapped "rustc"}";
+    extraFlagsVar = "NIX_RUSTFLAGS";
+  };
+
+  rustdoc-wrapper = replaceVars ./rustc-wrapper.sh {
+    inherit shell sysrootFlag defaultArgs;
+    prog = "${lib.getExe' rustc-unwrapped "rustdoc"}";
+    extraFlagsVar = "NIX_RUSTDOCFLAGS";
+  };
+in
 runCommand "${rustc-unwrapped.pname}-wrapper-${rustc-unwrapped.version}"
   {
     preferLocalBuild = true;
     strictDeps = true;
     inherit (rustc-unwrapped) outputs;
-
-    env = {
-      sysroot = lib.optionalString (sysroot != null) "--sysroot ${sysroot}";
-
-      # Upstream rustc still assumes that musl = static[1].  The fix for
-      # this is to disable crt-static by default for non-static musl
-      # targets.
-      #
-      # Even though Cargo will build build.rs files for the build platform,
-      # cross-compiling _from_ musl appears to work fine, so we only need
-      # to do this when rustc's target platform is dynamically linked musl.
-      #
-      # [1]: https://github.com/rust-lang/compiler-team/issues/422
-      #
-      # WARNING: using defaultArgs is dangerous, as it will apply to all
-      # targets used by this compiler (host and target).  This means
-      # that it can't be used to set arguments that should only be
-      # applied to the target.  It's fine to do this for -crt-static,
-      # because rustc does not support +crt-static host platforms
-      # anyway.
-      defaultArgs = lib.optionalString (
-        with rustc-unwrapped.stdenv.targetPlatform; isMusl && !isStatic
-      ) "-C target-feature=-crt-static";
-    };
 
     passthru = {
       inherit (rustc-unwrapped)
@@ -58,11 +70,8 @@ runCommand "${rustc-unwrapped.pname}-wrapper-${rustc-unwrapped.version}"
     mkdir -p $out/bin
     ln -s ${rustc-unwrapped}/bin/* $out/bin
     rm $out/bin/{rustc,rustdoc}
-    prog=${rustc-unwrapped}/bin/rustc extraFlagsVar=NIX_RUSTFLAGS \
-        substituteAll ${./rustc-wrapper.sh} $out/bin/rustc
-    prog=${rustc-unwrapped}/bin/rustdoc extraFlagsVar=NIX_RUSTDOCFLAGS \
-        substituteAll ${./rustc-wrapper.sh} $out/bin/rustdoc
-    chmod +x $out/bin/{rustc,rustdoc}
+    install -Dm755 ${rustc-wrapper} $out/bin/rustc
+    install -Dm755 ${rustdoc-wrapper} $out/bin/rustdoc
     ${lib.concatMapStrings (output: "ln -s ${rustc-unwrapped.${output}} \$${output}\n") (
       lib.remove "out" rustc-unwrapped.outputs
     )}
