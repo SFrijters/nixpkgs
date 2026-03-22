f7a7037905e67915349c212dc8af906f0fe96029
Stefan Frijters <sfrijters@gmail.com>
2026-03-22 11:31:28 +0100

    nixos/systemd-lib: avoid using passAsFile for structuredAttrs

--- nixos/lib/systemd-lib.nix
+++ nixos/lib/systemd-lib.nix
@@ -76,13 +76,16 @@ rec {
         {
           preferLocalBuild = true;
           allowSubstitutes = false;
-          __structuredAttrs = true;
+          # unit.text can be null. But variables that are null listed in
+          # passAsFile are ignored by nix, resulting in no file being created,
+          # making the mv operation fail.
+          text = optionalString (unit.text != null) unit.text;
+          textPath = builtins.toFile text;
         }
         ''
           name=${shellEscape name}
           mkdir -p "$out/$(dirname -- "$name")"
-          # unit.text can be null.
-          echo -n "${unit.text or ""}" > "$out/$name"
+          cp "$textPath" "$out/$name"
         ''
     else
       pkgs.runCommand "unit-${mkPathSafeName name}-disabled"
