f86b66be952a68d6816f7855566e4e6797743003
Stefan Frijters <sfrijters@gmail.com>
2026-07-24 20:32:41 +0200

    python3Packages.numpy: enable structuredAttrs

--- pkgs/development/python-modules/numpy/2.nix
+++ pkgs/development/python-modules/numpy/2.nix
@@ -231,6 +231,8 @@ buildPythonPackage (finalAttrs: {
     };
   };
 
+  __structuredAttrs = true;
+
   meta = {
     changelog = "https://github.com/numpy/numpy/releases/tag/${finalAttrs.src.tag}";
     description = "Scientific tools for Python";
