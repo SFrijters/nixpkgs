57a54ffe4623c885f5ef9741a524c12f86a8f3c6
Stefan Frijters <sfrijters@gmail.com>
2026-07-24 20:30:15 +0200

    libarchive: enable structuredAttrs

--- pkgs/by-name/li/libarchive/package.nix
+++ pkgs/by-name/li/libarchive/package.nix
@@ -140,6 +140,8 @@ stdenv.mkDerivation (finalAttrs: {
 
   enableParallelBuilding = true;
 
+  __structuredAttrs = true;
+
   meta = {
     homepage = "http://libarchive.org";
     description = "Multi-format archive and compression library";
