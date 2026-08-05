8c086d9af0af956b482ab6520789222430ec45a0
Stefan Frijters <sfrijters@gmail.com>
2026-08-05 23:12:32 +0200

    python3Packages.shiboken6-generator: enable strictDeps and structuredAttrs

--- pkgs/development/python-modules/shiboken6-generator/default.nix
+++ pkgs/development/python-modules/shiboken6-generator/default.nix
@@ -5,6 +5,7 @@
   python,
   cmake,
   stdenv,
+  buildPackages,
 }:
 
 let
@@ -41,6 +42,8 @@ stdenv'.mkDerivation (finalAttrs: {
     python.pkgs.qt6.qtbase
   ];
 
+  strictDeps = true;
+
   cmakeFlags = [
     "-Dis_pyside6_superproject_build=1"
   ];
@@ -50,9 +53,11 @@ stdenv'.mkDerivation (finalAttrs: {
   postInstall = ''
     cd ../../..
     chmod +w .
-    python3 setup.py egg_info --build-type=shiboken6-generator
+    python3 setup.py egg_info --build-type=shiboken6-generator --qtpaths=${lib.getExe' buildPackages.python3.pkgs.qt6.qtbase "qtpaths"}
     cp -r shiboken6_generator.egg-info $out/${python.sitePackages}/
   '';
+
+  __structuredAttrs = true;
 
   meta = {
     description = "Generator for the pyside6 Qt bindings - tools";
