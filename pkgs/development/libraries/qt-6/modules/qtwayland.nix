688bedf53d963ec56cb57c6e636bf2d6214e6069
Stefan Frijters <sfrijters@gmail.com>
2026-08-06 17:07:46 +0200

    WIP

--- pkgs/development/libraries/qt-6/modules/qtwayland.nix
+++ pkgs/development/libraries/qt-6/modules/qtwayland.nix
@@ -20,6 +20,9 @@ qtModule {
   # Conditional is required to prevent infinite recursion during a cross build
   cmakeFlags = lib.optionals (!stdenv.buildPlatform.canExecute stdenv.hostPlatform) [
     "-DQt6WaylandScannerTools_DIR=${pkgsBuildBuild.qt6.qtbase}/lib/cmake/Qt6WaylandScannerTools"
+  ]
+  ++ [
+    "-DQt6QuickTools_DIR=${pkgsBuildBuild.qt6.qtdeclarative}/lib/cmake/Qt6QuickTools"
   ];
 
   meta = {
