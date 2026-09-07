f7bb6dd755f42a5e89b287d5cef7d713f4a9d550
Stefan Frijters <sfrijters@gmail.com>
2026-09-08 00:00:20 +0200

    libpaper: enable structuredAttrs

--- pkgs/by-name/li/libpaper/package.nix
+++ pkgs/by-name/li/libpaper/package.nix
@@ -23,11 +23,9 @@ stdenv.mkDerivation (finalAttrs: {
   # as "${prefix}/etc", which is not expanded by AC_SUBST so libpaper will look
   # for config files in (literally, without expansion) '${prefix}/etc'. Manually
   # setting sysconfdir fixes this issue.
-  preConfigure = ''
-    configureFlagsArray+=(
-      "--sysconfdir=$out/etc"
-    )
-  '';
+  configureFlags = [
+    "--sysconfdir=${placeholder "out"}/etc"
+  ];
 
   # Set the default paper to letter (this is what libpaper uses as default as well,
   # if you call getdefaultpapername()).
@@ -36,6 +34,8 @@ stdenv.mkDerivation (finalAttrs: {
     mkdir -p $out/etc
     echo letter > $out/etc/papersize
   '';
+
+  __structuredAttrs = true;
 
   meta = {
     changelog = "https://github.com/rrthomas/libpaper/releases/tag/v${finalAttrs.version}";
