51bf767123d22148297f743fe7016cfe5d13304e
Stefan Frijters <sfrijters@gmail.com>
2026-09-09 14:26:44 +0200

    tests.testers.runCommand.bork: skip failing tests

--- pkgs/development/python-modules/bork/tests.nix
+++ pkgs/development/python-modules/bork/tests.nix
@@ -21,6 +21,12 @@
       cp -r ${bork.src} bork/
       find -type d -exec chmod 0755 '{}' '+'
 
+      # Exclude the homf repository because it uses hatchling as a build system
+      # and creating the appropriate environment fails.
+      # This is not something we *need* to exercise runCommand
+      substituteInPlace bork/tests/conftest.py \
+        --replace-fail '"https://github.com/duckinator/homf.git",' ' '
+
       pytest -v -m network bork/
       touch $out
     '';
